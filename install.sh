110299bc1e90a22807b62a59d88bfe880af04cd3
modernpacifist <modernpacifist@gmail.com>
2022-08-01 13:10:56 +0300

    install.sh:added user privileges check

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if [[ $EUID != 0 ]]; then
+    echo 'You need superuser privileges for installation'
+    exit 1
+fi
+
 find ./src/ -name "__*" -exec cp {} /bin/ \; 2>/dev/null
